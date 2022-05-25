b0e14cd9bb750bba4ddb13729456284be0cafc6a
PoglianiMattia <mattia.pogliani@gmail.com>
2022-05-25 08:33:16 +0200

    optimize code

--- es3.js
+++ es3.js
@@ -1,70 +1,51 @@
 var count = 2;
 $("#btn1").click(function () {
-  count++;
-  var n1 = +$("#N1").val();
-  var n2 = +$("#N2").val();
-  //console.log(n1 -(-n2));
-  console.log(n1 + n2);
-  $("#table").append(
-    "<tr><td>" +
-      count +
-      "</td><td>" +
-      n1 +
-      "</td><td>+</td><td>" +
-      n2 +
-      "</td><td>" +
-      (n1 + n2) +
-      "</td></tr>"
-  );
+  calculation("+");
 });
 $("#btn2").click(function () {
-  count++;
-  var n1 = +$("#N1").val();
-  var n2 = +$("#N2").val();
-  console.log(n1 - n2);
-  $("#table").append(
-    "<tr><td>" +
-      count +
-      "</td><td>" +
-      n1 +
-      "</td><td>-</td><td>" +
-      n2 +
-      "</td><td>" +
-      (n1 - n2) +
-      "</td></tr>"
-  );
+  calculation("-");
 });
 $("#btn3").click(function () {
-  count++;
-  var n1 = +$("#N1").val();
-  var n2 = +$("#N2").val();
-  console.log(n1 * n2);
-  $("#table").append(
-    "<tr><td>" +
-      count +
-      "</td><td>" +
-      n1 +
-      "</td><td>*</td><td>" +
-      n2 +
-      "</td><td>" +
-      n1 * n2 +
-      "</td></tr>"
-  );
+  calculation("*");
 });
 $("#btn4").click(function () {
-  count++;
-  var n1 = +$("#N1").val();
-  var n2 = +$("#N2").val();
-  console.log(n1 / n2);
-  $("#table").append(
-    "<tr><td>" +
-      count +
-      "</td><td>" +
-      n1 +
-      "</td><td>/</td><td>" +
-      n2 +
-      "</td><td>" +
-      n1 / n2 +
-      "</td></tr>"
-  );
+  calculation("/");
 });
+
+function calculation(sign) {
+  if ($("#N1").val() && $("#N2").val()) {
+    count++;
+    var n1 = +$("#N1").val();
+    var n2 = +$("#N2").val();
+    var res = n1 + n2;
+    switch (sign) {
+      case "+":
+        var res = n1 + n2;
+        // n1 -(-n2) ;
+        break;
+      case "-":
+        var res = n1 - n2;
+        break;
+      case "*":
+        var res = n1 * n2;
+        break;
+      case "/":
+        var res = n1 / n2;
+        break;
+    }
+    console.log(res);
+    $("#table").append(
+      "<tr><td>" +
+        count +
+        "</td><td>" +
+        n1 +
+        "</td><td>" +
+        sign +
+        "</td><td>" +
+        n2 +
+        "</td><td>" +
+        res +
+        "</td></tr>"
+    );
+  } else alert("fill all the text field");
+}
